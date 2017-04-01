5be89695095fbf186d84950711d7357056bc3e23
anqqa <anttiq@gmail.com>
2017-04-01 13:19:54 +0300

    Renaming

--- android/app/src/main/java/org/klubitus/MainActivity.java
+++ android/app/src/main/java/org/klubitus/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactklubitus;
+package org.klubitus;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactKlubitus";
+        return "KlubitusAndroid";
     }
 }
