7ac20959a6dc6d88e8817c1b93ab925351c9e56e
anqqa <anttiq@gmail.com>
2017-04-03 12:54:47 +0300

    Hello World for web

---
+++ app/web/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class ReactKlubitus extends Component {
+  render() {
+    return (
+      <div className="react">
+        Klubitus
+      </div>
+    );
+  }
+}
+
+ReactKlubitus.propTypes = {
+
+};
+
+export default ReactKlubitus;
